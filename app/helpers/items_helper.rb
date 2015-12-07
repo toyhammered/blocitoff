61415b8bb229e74d3844fed69458bea586c5a502
toyhammered <daniel@rassiner.com>
2015-12-06 18:26:16 -0800

    completed fixing time issue, and update_attributes

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,5 +1,5 @@
 module ItemsHelper
   def time_remaining(task)
-    distance_of_time_in_words(task.created_at - 7.days, Time.now)
+    distance_of_time_in_words(7.days.ago, task.created_at.to_time)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
+
+  default_scope { order('created_at DESC')}
 end
